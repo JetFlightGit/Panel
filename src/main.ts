41d44b6929e1f482289757c1230a007e8e1e383b
Clemiinator <clemens.sigel@gmail.com>
2023-03-20 22:24:23 +0100

    Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -13,5 +14,5 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-app.use(router)
+app.use(router).use(createPinia())
 app.mount('#app')
